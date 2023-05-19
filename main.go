7467c1ac276087417b4025071f88662590362033
Ricardo Antonio Valladares Renderos <R_A_V_R_@HOTMAIL.COM>
2023-05-19 07:36:37 -0600

    Update main.go

--- main.go
+++ main.go
@@ -1,9 +1,9 @@
 package main
 
 import saludo "github.com/RicardoValladares/Golang/08-librerias/libreria"
+import "fmt"
 
 func main() {
 	saludo.Hola()
-	saludo.Hola()
-	saludo.Hola()
+	fmt.Println("Hola")
 }
